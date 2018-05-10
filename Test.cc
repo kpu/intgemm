6f0fd704a51477280c0640c09b28017122997afa
Kenneth Heafield <github@kheafield.com>
2018-05-10 22:29:52 +0000

    Change to timing

--- Test.cc
+++ Test.cc
@@ -63,22 +63,7 @@ void SlowRef_16(const int16_t * A, const int16_t * B, float * C, float quant_mul
     }
 }
 
-// Program takes no input
-int main(int argc, char ** argv) {
-    std::srand(45678);
-
-    // A is usually an activation matrix, B is usually a weight matrix.
-    // We actually compute A * B^T. num_B_rows is the rows in B^T. 
-    int num_A_rows = 5;
-    int num_B_rows = 512;
-    // This is the shared dimension.
-    int width = 1024;
-
-    printf("Computing matrix multiplication: %d x %d x %d\n", num_A_rows, width, num_B_rows);
-    
-    //assert(num_A_rows % 4 == 0);
-    assert(width % 8 == 0);
-
+void Time(int num_A_rows, int num_B_rows, int width) {
     float * A = static_cast<float*>(aligned_alloc(64, sizeof(float) * num_A_rows * width));
     float * B = static_cast<float*>(aligned_alloc(64, sizeof(float) * num_B_rows * width));
     
@@ -104,70 +89,33 @@ int main(int argc, char ** argv) {
     // So we must divide by 1.0/(n^2) to get back the original value.
     double unquant_mult = 1.0/(quant_mult*quant_mult);
 
-/*    float * SSE_C = new float[num_A_rows*num_B_rows];
-    {
-      StopWatch w("SSE quantization");
-      // The weight matrix should be quantized before starting decoding, since it is known beforehand.
-      SSE_Quantize(B, (__m128i *)quant_B, (float)quant_mult, num_B_rows, width);
-      // The activation matrix must be quantized on-the-fly.
-      SSE_Quantize(A, (__m128i *)quant_A, (float)quant_mult, num_A_rows, width);
-    }
-    {
-      StopWatch w("SSE matrix multiply");  
-      SSE_MatrixMult((__m128i*)quant_A, (__m128i*)quant_B, SSE_C, (float)unquant_mult, num_A_rows, num_B_rows, width);
-    }*/
-
+    // The weight matrix should be quantized before starting decoding, since it is known beforehand.
+    AVX_Quantize(B, (__m256i *)quant_B, (float)quant_mult, num_B_rows * width);
+    // The activation matrix must be quantized on-the-fly.
+    AVX_Quantize(A, (__m256i *)quant_A, (float)quant_mult, num_A_rows * width);
     float * AVX_C = new float[num_A_rows*num_B_rows];
+    // Burn in
+    AVX_MatrixMult(quant_A, quant_B, AVX_C, (float)unquant_mult, num_A_rows, num_B_rows, width);
     {
-      StopWatch w("AVX quantization");
-      // The weight matrix should be quantized before starting decoding, since it is known beforehand.
-      AVX_Quantize(B, (__m256i *)quant_B, (float)quant_mult, num_B_rows * width);
-      // The activation matrix must be quantized on-the-fly.
-      AVX_Quantize(A, (__m256i *)quant_A, (float)quant_mult, num_A_rows * width);
+      StopWatch w("AVX ");
+      for (int i = 0; i < 10; ++i)
+        AVX_MatrixMult(quant_A, quant_B, AVX_C, (float)unquant_mult, num_A_rows, num_B_rows, width);
     }
-    {
-      StopWatch w("AVX multiply");
-      AVX_MatrixMult(quant_A, quant_B, AVX_C, (float)unquant_mult, num_A_rows, num_B_rows, width);
-    }
-
-    // C will thus be num_A_rows x num_B_rows
-    float * ref_C = new float[num_A_rows*num_B_rows];
-    {
-      StopWatch w("Reference int16_t multiply");
-      SlowRef_16((int16_t*)quant_A, (int16_t*)quant_B, ref_C, (float)unquant_mult, num_A_rows, num_B_rows, width);
-    }
-
-
 
     free(A);
     free(B);
     free(quant_A);
     free(quant_B);
-    
-    double max_diff = 0.0;
-    double mean_diff = 0.0;
-    for (int i = 0; i < num_A_rows; i++) {
-        for (int j = 0; j < num_B_rows; j++) {
-            float r = ref_C[i*num_B_rows + j];
-            float f = AVX_C[i*num_B_rows + j];
-            double diff = std::fabs(r-f);
-            if (diff > max_diff) {
-                max_diff = diff;
-            }
-            mean_diff += diff;
-        }
-    }
-
-    delete [] ref_C;
-//    delete [] SSE_C;
     delete [] AVX_C;
-    
-    mean_diff /= (double)num_A_rows*(double)num_B_rows;
+}
 
-    std::printf("Diff between AVX512 and ref:\n");
-    std::printf("  Mean = %g\n", mean_diff);
-    std::printf("  Max = %g\n", max_diff);
-    
+// Program takes no input
+int main(int argc, char ** argv) {
+    std::srand(45678);
+    Time(512, 1024, 1024);    
+    Time(1024, 2048, 1024);    
+    Time(64, 64, 64);    
+    Time(64, 64, 512);
     return 0;
 }
 
