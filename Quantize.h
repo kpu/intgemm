b0a32a1c10f912a2cb91db0e7e2647cab2a5ca91
kpu <github@kheafield.com>
2018-06-09 20:38:44 +0000

    Fix define for AVX512

--- Quantize.h
+++ Quantize.h
@@ -5,12 +5,12 @@
 
 namespace intgemm {
 
-#ifdef __AVX512__
+#ifdef __AVX512F__
 namespace AVX512 {
 void Quantize16(const float *input, int16_t *output, float quant_mult, std::size_t size);
 void Quantize8(const float *input, int8_t *output, float quant_mult, std::size_t size);
 } // namespace AVX512
-#endif
+#endif // __AVX512F__
 
 #ifdef __AVX2__
 namespace AVX2 {
