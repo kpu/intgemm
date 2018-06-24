bb89fe0178bf34f2bdf3da0a6dc4e21f20e01d78
Kenneth Heafield <github@kheafield.com>
2018-06-24 19:18:55 +0100

    rtsdcp

--- stop_watch.h
+++ stop_watch.h
@@ -1,20 +1,28 @@
 #pragma once
 #include <stdint.h>
+#include <cstdlib>
 #include <vector>
 #include <x86intrin.h>
+#include <iostream>
 
 class StopWatch {
   public:
     StopWatch(std::vector<uint64_t> &stats)
-      : stats_(stats), start_(__rdtsc()) {}
+      : stats_(stats), start_(__rdtscp(&processor_)) {}
 
     ~StopWatch() {
-      uint64_t stop = __rdtsc();
+      unsigned int proc;
+      uint64_t stop = __rdtscp(&proc);
+      if (proc != processor_) {
+        std::cerr << "Detected core change from " << processor_ << " to " << proc << std::endl;
+        abort();
+      }
       stats_.push_back(stop - start_);
     }
 
   private:
     std::vector<uint64_t> &stats_;
+    uint32_t processor_;
     uint64_t start_;
 };
 
