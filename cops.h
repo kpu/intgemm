6104c19920d2986c3d5e74d2cf42547781813d12
Nikolay Bogoychev <nheart@gmail.com>
2019-04-17 19:08:46 +0100

    Function multi-versioning is broken AF on gcc

--- cops.h
+++ cops.h
@@ -13,32 +13,31 @@ public:
  AVX2 inline void operator()(Index rowIDX, Index cols, Index colIDX, __m256i result);
 
 private:
-  DEFAULT void InitRegister(float unquant_mult);
-  SSE2 void InitRegister(float unquant_mult);
-  AVX2 void InitRegister(float unquant_mult);
+  SSE2 void InitRegisterSSE(float unquant_mult);
+  AVX2 void InitRegisterAVX2(float unquant_mult);
 
   float * C_;
   __m128 unquant_mult_128; // Registers
   __m256 unquant_mult_256;
 };
 
-DEFAULT void JustUnquantizeC::InitRegister(float) {
-  throw UnsupportedCPU();
-}
-
-SSE2 void JustUnquantizeC::InitRegister(float unquant_mult) {
+SSE2 void JustUnquantizeC::InitRegisterSSE(float unquant_mult) {
   assert(reinterpret_cast<uintptr_t>(C_) % sizeof(__m128) == 0);
   unquant_mult_128 = _mm_set1_ps(unquant_mult);
 }
 
-AVX2 void JustUnquantizeC::InitRegister(float unquant_mult) {
+AVX2 void JustUnquantizeC::InitRegisterAVX2(float unquant_mult) {
   assert(reinterpret_cast<uintptr_t>(C_) % sizeof(__m256) == 0);
-  unquant_mult_128 = _mm_set1_ps(unquant_mult);
   unquant_mult_256 = _mm256_set1_ps(unquant_mult);
 }
 
 JustUnquantizeC::JustUnquantizeC(float *C, float unquant_mult) : C_(C) {
-  InitRegister(unquant_mult);
+  //We need both to make sure our tests pass
+  //Some of the assertions might give false positives on SSE2/3
+  InitRegisterSSE(unquant_mult);
+  if (__builtin_cpu_supports("avx2")) {
+    InitRegisterAVX2(unquant_mult);
+  }
 }
 
 
