f9bbff340eb454ad7995737596028fabfdaa81f7
Nikolay Bogoychev <nheart@gmail.com>
2019-05-21 15:38:36 +0100

    Backport Mateusz fix

--- types.h
+++ types.h
@@ -33,7 +33,7 @@ class UnsupportedCPU : public std::exception {
 typedef unsigned int Index;
 
 // If you want to detect the CPU and dispatch yourself, here's what to use:
-typedef enum {CPU_AVX512BW = 4, CPU_AVX2 = 3, CPU_SSSE3 = 2, CPU_SSE2 = 1, CPU_UNSUPPORTED} CPUType;
+typedef enum {CPU_AVX512BW = 4, CPU_AVX2 = 3, CPU_SSSE3 = 2, CPU_SSE2 = 1, CPU_UNSUPPORTED = 0} CPUType;
 
 // Running CPU type.  This is defined in intgemm.cc (as the dispatcher).
 extern const CPUType kCPU;
