cd43bb5faf3feb3c8b8aa5754abd23ab2833d522
Mateusz Chudyk <mateuszchudyk@gmail.com>
2019-05-21 17:25:35 +0100

    Add ReLU postprocess

--- cops.h
+++ cops.h
@@ -84,4 +84,70 @@ class Identity {
     int32_t *C_;
 };
 
+class ReLU {
+  public:
+    explicit ReLU(int32_t* C) : C_(C) {}
+
+    class OnSSE2 {
+      public:
+        INTGEMM_SSE2 explicit OnSSE2(const ReLU& from)
+          : C_(from.C_), zeros_(set1_epi32<__m128i>(0)) {
+          assert(reinterpret_cast<uintptr_t>(C_) % sizeof(__m128i) == 0);
+        }
+
+        INTGEMM_SSE2 inline void operator()(Index rowIDX, Index cols, Index colIDX, MultiplyResult128 result) {
+          __m128i positive0124 = _mm_cmplt_epi32(zeros_, result.pack0123);
+          _mm_storeu_si128(reinterpret_cast<__m128i*>(C_ + rowIDX*cols + colIDX), zeros_);
+          _mm_maskmoveu_si128(result.pack0123, positive0124, reinterpret_cast<char*>(C_ + rowIDX*cols + colIDX));
+
+          __m128i positive4567 = _mm_cmplt_epi32(zeros_, result.pack4567);
+          _mm_storeu_si128(reinterpret_cast<__m128i*>(C_ + rowIDX*cols + colIDX + 4), zeros_);
+          _mm_maskmoveu_si128(result.pack4567, positive4567, reinterpret_cast<char*>(C_ + rowIDX*cols + colIDX + 4));
+        }
+
+      private:
+        int32_t *C_;
+        __m128i zeros_;
+    };
+
+    using OnSSSE2 = OnSSE2;
+
+    class OnAVX2 {
+      public:
+        INTGEMM_AVX2 explicit OnAVX2(const ReLU& from)
+          : C_(from.C_), zeros_(set1_epi32<__m256i>(0)) {
+          assert(reinterpret_cast<uintptr_t>(C_) % sizeof(__m256i) == 0);
+        }
+
+        INTGEMM_AVX2 inline void operator()(Index rowIDX, Index cols, Index colIDX, __m256i result) {
+          __m256i nonnegative = _mm256_max_epi32(zeros_, result);
+          _mm256_storeu_si256(reinterpret_cast<__m256i*>(C_ + rowIDX*cols + colIDX), nonnegative);
+        }
+
+      private:
+        int32_t *C_;
+        __m256i zeros_;
+    };
+
+    class OnAVX512 {
+      public:
+        INTGEMM_AVX512BW explicit OnAVX512(const ReLU& from)
+          : C_(from.C_), zeros_(set1_epi32<__m512i>(0)) {
+          assert(reinterpret_cast<uintptr_t>(C_) % sizeof(__m512i) == 0);
+        }
+
+        INTGEMM_AVX512BW inline void operator()(Index rowIDX, Index cols, Index colIDX, __m512i result) {
+          __m512i nonnegative = _mm512_max_epi32(zeros_, result);
+          _mm512_storeu_si512(reinterpret_cast<__m512i*>(C_ + rowIDX*cols + colIDX), nonnegative);
+        }
+
+      private:
+        int32_t *C_;
+        __m512i zeros_;
+    };
+
+  private:
+    int32_t *C_;
+};
+
 } //Namespace
--- intrinsics.h
+++ intrinsics.h
@@ -13,11 +13,18 @@ namespace intgemm {
  * templates.
  */
 template <class Register> static inline Register set1_epi16(int16_t to);
+template <class Register> static inline Register set1_epi32(int32_t to);
 template <class Register> static inline Register set1_ps(float to);
 
 struct MultiplyResult128 {
   __m128i pack0123, pack4567;
 };
+
+//------------------------------------------------------------------------------
+//
+// SSE2
+//
+//------------------------------------------------------------------------------
 INTGEMM_SSE2 static inline __m128i add_epi32(__m128i first, __m128i second) {
   return _mm_add_epi32(first, second);
 }
@@ -26,6 +33,9 @@ INTGEMM_SSE2 static inline __m128i adds_epi16(__m128i first, __m128i second) {
 }
 template <> INTGEMM_SSE2 inline __m128i set1_epi16<__m128i>(int16_t to) {
   return _mm_set1_epi16(to);
+}
+template <> INTGEMM_SSE2 inline __m128i set1_epi32<__m128i>(int32_t to) {
+  return _mm_set1_epi32(to);
 }
 template <> INTGEMM_SSE2 inline __m128 set1_ps<__m128>(float to) {
   return _mm_set1_ps(to);
@@ -39,7 +49,6 @@ INTGEMM_SSSE3 static inline __m128i maddubs_epi16(__m128i first, __m128i second)
 INTGEMM_SSSE3 static inline __m128i sign_epi8(__m128i first, __m128i second) {
   return _mm_sign_epi8(first, second);
 }
-
 INTGEMM_SSSE3 static inline __m128i abs_epi8(__m128i arg) {
   return _mm_abs_epi8(arg);
 }
@@ -52,10 +61,15 @@ INTGEMM_SSE2 static inline __m128 and_ps(__m128 first, __m128 second) {
 INTGEMM_SSE2 static inline __m128 cvtepi32_ps(__m128i arg) {
   return _mm_cvtepi32_ps(arg);
 }
-INTGEMM_SSE2 static inline __m128 mul_ps (__m128 a, __m128 b) {
+INTGEMM_SSE2 static inline __m128 mul_ps(__m128 a, __m128 b) {
   return _mm_mul_ps(a, b);
 }
 
+//------------------------------------------------------------------------------
+//
+// AVX2
+//
+//------------------------------------------------------------------------------
 INTGEMM_AVX2 static inline __m256i add_epi32(__m256i first, __m256i second) {
   return _mm256_add_epi32(first, second);
 }
@@ -64,6 +78,9 @@ INTGEMM_AVX2 static inline __m256i adds_epi16(__m256i first, __m256i second) {
 }
 template <> INTGEMM_AVX2 inline __m256i set1_epi16<__m256i>(int16_t to) {
   return _mm256_set1_epi16(to);
+}
+template <> INTGEMM_AVX2 inline __m256i set1_epi32<__m256i>(int32_t to) {
+  return _mm256_set1_epi32(to);
 }
 template <> INTGEMM_AVX2 inline __m256 set1_ps<__m256>(float to) {
   return _mm256_set1_ps(to);
@@ -93,12 +110,21 @@ INTGEMM_AVX2 static inline __m256 mul_ps (__m256 a, __m256 b) {
   return _mm256_mul_ps(a, b);
 }
 
+//------------------------------------------------------------------------------
+//
+// AVX512
+//
+//------------------------------------------------------------------------------
 #ifndef INTGEMM_NO_AVX512
+
 INTGEMM_AVX512BW static inline __m512i add_epi32(__m512i first, __m512i second) {
   return _mm512_add_epi32(first, second);
 }
 template <> inline INTGEMM_AVX512BW __m512i set1_epi16<__m512i>(int16_t to) {
   return _mm512_set1_epi16(to);
+}
+template <> inline INTGEMM_AVX512BW __m512i set1_epi32<__m512i>(int32_t to) {
+  return _mm512_set1_epi32(to);
 }
 template <> inline INTGEMM_AVX512BW __m512 set1_ps<__m512>(float to) {
   return _mm512_set1_ps(to);
@@ -119,5 +145,7 @@ INTGEMM_AVX512BW static inline __m512 max_ps(__m512 first, __m512 second) {
 INTGEMM_AVX512DQ static inline __m512 and_ps(__m512 first, __m512 second) {
   return _mm512_and_ps(first, second);
 }
+
 #endif
+
 } //namespace
