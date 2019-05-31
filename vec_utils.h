7424a95c7ce7e5cf495b0e14806a90e780e5dd71
Mateusz Chudyk <mateuszchudyk@gmail.com>
2019-05-31 13:02:11 +0100

    Change comment style in vec_utils.h

--- vec_utils.h
+++ vec_utils.h
@@ -8,36 +8,36 @@ struct MultiplyResult128 {
   __m128i pack0123, pack4567;
 };
 
-//------------------------------------------------------------------------------
-//
-// Quantize
-//
-//------------------------------------------------------------------------------
+/*
+ *
+ * Quantize
+ *
+ */
 INTGEMM_SSE2 static inline __m128i quantize(__m128 input, __m128 quantize_mult) {
   return cvtps_epi32(mul_ps(input, quantize_mult));
 }
-INTGEMM_AVX2 static inline __m256i quantize(__m256 input, __m256 quantize_mult){
+INTGEMM_AVX2 static inline __m256i quantize(__m256 input, __m256 quantize_mult) {
   return cvtps_epi32(mul_ps(input, quantize_mult));
 }
 #ifndef INTGEMM_NO_AVX512
-INTGEMM_AVX512BW static inline __m512i quantize(__m512 input, __m512 quantize_mult){
+INTGEMM_AVX512BW static inline __m512i quantize(__m512 input, __m512 quantize_mult) {
   return cvtps_epi32(mul_ps(input, quantize_mult));
 }
 #endif
 
-//------------------------------------------------------------------------------
-//
-// Unquantize
-//
-//------------------------------------------------------------------------------
+/*
+ *
+ * Unquantize
+ *
+ */
 INTGEMM_SSE2 static inline __m128 unquantize(__m128i input, __m128 unquantize_mult) {
   return mul_ps(cvtepi32_ps(input), unquantize_mult);
 }
-INTGEMM_AVX2 static inline __m256 unquantize(__m256i input, __m256 unquantize_mult){
+INTGEMM_AVX2 static inline __m256 unquantize(__m256i input, __m256 unquantize_mult) {
   return mul_ps(cvtepi32_ps(input), unquantize_mult);
 }
 #ifndef INTGEMM_NO_AVX512
-INTGEMM_AVX512BW static inline __m512 unquantize(__m512i input, __m512 unquantize_mult){
+INTGEMM_AVX512BW static inline __m512 unquantize(__m512i input, __m512 unquantize_mult) {
   return mul_ps(cvtepi32_ps(input), unquantize_mult);
 }
 #endif
