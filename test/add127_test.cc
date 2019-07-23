ad7c081c18a8a2dd86324be57b9c8f9d892bcebe
Nikolay Bogoychev <nheart@gmail.com>
2019-07-23 11:05:22 +0100

    Sanity check with 16 bit, not passing

--- test/add127_test.cc
+++ test/add127_test.cc
@@ -135,6 +135,73 @@ template <class Routine> void TestMultiplyBiasNew(Index A_rows, Index width, Ind
    int_tolerance, float_tolerance, MSE_float_tolerance, MSE_int_tolerance);
 }
 
+template <class Routine> void TestMultiplyBiasNew16(Index A_rows, Index width, Index B_cols,
+ float int_tolerance=.1, float float_tolerance=1, float MSE_float_tolerance=0, float MSE_int_tolerance=0) {
+  std::ostringstream info;
+  info << Routine::kName << "\t" << A_rows << '\t' << width << '\t' << B_cols << '\n';
+
+  // Initialize A and B.
+  AlignedVector<float> A(A_rows * width);
+  AlignedVector<float> B(width * B_cols);
+  AlignedVector<float> bias(B_cols);
+  std::mt19937 gen;
+  std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+  for (auto& it : A) {
+    it = dist(gen);
+  }
+  for (auto& it : B) {
+    it = dist(gen);
+  }
+  for (auto& it : bias) {
+    it = dist(gen);
+  }
+  
+  float alpha = 2.0f;
+  float quant_mult = 127/alpha;
+  float unquant_mult = 1.0/(quant_mult*quant_mult);
+
+  AlignedVector<uint8_t> A_prep(A.size());
+  AlignedVector<int8_t> B_prep(B.size());
+  AVX2_8bit::PrepareA(A.begin(), A_prep.begin(), quant_mult, A_rows, width);
+  AVX2_8bit::PrepareB(B.begin(), B_prep.begin(), quant_mult, width, B_cols);
+
+  AlignedVector<float> test_C(A_rows * B_cols);
+
+  /*REFERENCE MULTIPLICATION
+  *
+  *
+  */
+  AlignedVector<int8_t> B_quant(B.size());
+  AVX2_8bit::Quantize(B.begin(), B_quant.begin(), quant_mult, B.size());
+  AlignedVector<float> slowint_C(test_C.size());
+  // Taking the original A_preparation which means A would be int8_t
+  AlignedVector<int8_t> A_prep2(A.size());
+  AVX2_8bit::PrepareA(A.begin(), A_prep2.begin(), quant_mult, A_rows, width);
+  SlowRefInt(A_prep2.begin(), B_quant.begin(), slowint_C.begin(), unquant_mult, A_rows, width, B_cols, bias.begin());
+
+  AlignedVector<float> float_C(test_C.size());
+  SlowRefFloat(A.begin(), B.begin(), float_C.begin(), A_rows, width, B_cols, bias.begin());
+
+  /*ACTUAL MULTIPLICATION
+  *
+  *
+  */
+  AlignedVector<int16_t> A_prep16(A.size());
+  AlignedVector<int16_t> B_prep16(B.size());
+  for (size_t i = 0; i < A_prep16.size(); i++) {
+    A_prep16[i] = (int16_t)A_prep[i];
+  }
+
+  Routine::PrepareB(B.begin(), B_prep16.begin(), quant_mult, width, B_cols);
+
+
+  AVX2_8bit::PrepareBiasFor8(B.begin(), bias.begin(), alpha, width, B_cols);
+  Routine::Multiply(A_prep16.begin(), B_prep16.begin(), BiasAddUnquantizeC(test_C.begin(), bias.begin(), unquant_mult), A_rows, width, B_cols);
+
+  Compare(float_C.begin(), slowint_C.begin(), test_C.begin(), test_C.size(), info.str(),
+   int_tolerance, float_tolerance, MSE_float_tolerance, MSE_int_tolerance);
+}
+
 // Bias
 TEST_CASE("PrepareBias SSSE3", "[Add127]") {
 	if (kCPU < CPU_SSSE3) return;
@@ -222,6 +289,40 @@ TEST_CASE ("Multiply AVX512F 8bit with new bias", "[Add127]") {
   TestMultiplyBiasNew<AVX512_8bit>(472, 256, 256, 0.46, 0.62, 0.17, 0.16); //0.1, 0);
   TestMultiplyBiasNew<AVX512_8bit>(248, 256, 256, 0.48, 0.64, 0.16, 0.15); //0.1, 0);
   TestMultiplyBiasNew<AVX512_8bit>(200, 256, 256, 0.57, 0.74, 0.17, 0.16); //0.1, 0);
+}
+
+TEST_CASE ("Multiply SSSE3 16bit with new bias", "[Add127]") {
+  if (kCPU < CPU_SSSE3) return;
+  TestMultiplyBiasNew16<SSE2_16bit>(1, 64, 8, 0.11, 0.1, 0.06, 0.05);
+  TestMultiplyBiasNew16<SSE2_16bit>(8, 256, 256, 0.45, 0.54, 0.17, 0.16); // 0.064, 0.026);
+  TestMultiplyBiasNew16<SSE2_16bit>(8, 2048, 256, 1.7, 1.7, 0.46, 0.43); // 4.4, 4.4);
+  TestMultiplyBiasNew16<SSE2_16bit>(320, 256, 256, 0.56, 0.64, 0.16, 0.15); // 0.1, 0.01);
+  TestMultiplyBiasNew16<SSE2_16bit>(472, 256, 256, 0.46, 0.62, 0.17, 0.16); // 0.1, 0.011);
+  TestMultiplyBiasNew16<SSE2_16bit>(248, 256, 256, 0.48, 0.64, 0.16, 0.15); // 0.1, 0.012);
+  TestMultiplyBiasNew16<SSE2_16bit>(200, 256, 256, 0.55, 0.74, 0.17, 0.16); // 0.1, 0.011);*/
+}
+
+
+TEST_CASE ("Multiply AVX2 16bit with new bias", "[Add127]") {
+  if (kCPU < CPU_AVX2) return;
+  TestMultiplyBiasNew16<AVX2_16bit>(1, 64, 8, 0.11, 0.11, 0.01, 0.05);
+  TestMultiplyBiasNew16<AVX2_16bit>(8, 256, 256, 0.49, 0.54, 0.17, 0.16); //0.1, 0);
+  TestMultiplyBiasNew16<AVX2_16bit>(8, 2048, 256, 1.57, 1.66, 0.46, 0.46); //1.8, 1.8);
+  TestMultiplyBiasNew16<AVX2_16bit>(320, 256, 256, 0.49, 0.64, 0.16, 0.15); //0.1, 0);
+  TestMultiplyBiasNew16<AVX2_16bit>(472, 256, 256, 0.46, 0.62, 0.17, 0.16); //0.1, 0);
+  TestMultiplyBiasNew16<AVX2_16bit>(248, 256, 256, 0.48, 0.64, 0.16, 0.15); //0.1, 0);
+  TestMultiplyBiasNew16<AVX2_16bit>(200, 256, 256, 0.55, 0.74, 0.17, 0.16); //0.1, 0);
+}
+
+TEST_CASE ("Multiply AVX512F 16bit with new bias", "[Add127]") {
+  if (kCPU < CPU_AVX512BW) return;
+  TestMultiplyBiasNew16<AVX512_16bit>(1, 64, 8, 0.11, 0.11, 0.06, 0.05);
+  TestMultiplyBiasNew16<AVX512_16bit>(8, 256, 256, 0.48, 0.54, 0.17, 0.16); //, 1.6, 1.6); //0.1, 0);
+  TestMultiplyBiasNew16<AVX512_16bit>(8, 2048, 256, 1.57, 1.66, 0.46, 0.43); //1.8, 1.8);
+  TestMultiplyBiasNew16<AVX512_16bit>(320, 256, 256, 0.48, 0.64, 0.16, 0.15); //0.1, 0);
+  TestMultiplyBiasNew16<AVX512_16bit>(472, 256, 256, 0.46, 0.62, 0.17, 0.16); //0.1, 0);
+  TestMultiplyBiasNew16<AVX512_16bit>(248, 256, 256, 0.48, 0.64, 0.16, 0.15); //0.1, 0);
+  TestMultiplyBiasNew16<AVX512_16bit>(200, 256, 256, 0.57, 0.74, 0.17, 0.16); //0.1, 0);
 }
 
 } //namespace intgemm
