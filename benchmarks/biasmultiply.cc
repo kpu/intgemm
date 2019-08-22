7601b0a5d8535d49b8129cf6b045bcdfe86d555d
Nikolay Bogoychev <nheart@gmail.com>
2019-08-22 18:49:30 +0100

    Update the benchmark

--- benchmarks/biasmultiply.cc
+++ benchmarks/biasmultiply.cc
@@ -87,11 +87,56 @@ std::chrono::duration<double> testOld(Index A_rows, Index width, Index B_cols) {
 
 }
 
+template <class Routine>
+std::chrono::duration<double> testOld_nobias(Index A_rows, Index width, Index B_cols) {
+  AlignedVector<float> A(A_rows * width);
+  AlignedVector<float> B(width * B_cols);
+  std::mt19937 gen;
+  std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+  for (auto& it : A) {
+    it = dist(gen);
+  }
+  for (auto& it : B) {
+    it = dist(gen);
+  }
+  
+  float alpha = 2.0f;
+  float quant_mult = 127/alpha;
+  float unquant_mult = 1.0/(quant_mult*quant_mult);
+
+  AlignedVector<int8_t> A_prep(A.size());
+  AlignedVector<int8_t> B_prep(B.size());
+  Routine::PrepareA(A.begin(), A_prep.begin(), quant_mult, A_rows, width);
+  Routine::PrepareB(B.begin(), B_prep.begin(), quant_mult, width, B_cols);
+
+  AlignedVector<float> test_C(A_rows * B_cols);
+
+  auto start = std::chrono::system_clock::now();
+  Routine::Multiply(A_prep.begin(), B_prep.begin(), A_rows, width, B_cols, callbacks::UnquantizeAndWrite(unquant_mult, test_C.begin()));
+  auto end = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> elapsed_seconds = end-start;
+  return elapsed_seconds;
+
+}
+
 int main(int argc, char ** argv) {
 	int repeat = 1000;
 	if (argc > 1) {
 		repeat = atoi(argv[1]);
 	}
+
+	std::chrono::duration<double> oldSSSE3_nobias = testOld_nobias<SSSE3_8bit>(1, 64, 8);
+	for (int i = 0; i<repeat; i++) {
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(8, 256, 256);
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(8, 2048, 256);
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(320, 256, 256);
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(472, 256, 256);
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(248, 256, 256);
+		oldSSSE3_nobias += testOld_nobias<SSSE3_8bit>(200, 256, 256);
+	}
+
+	std::cout << repeat << " iterations of Old SSSE3 without bias took: " << oldSSSE3_nobias.count() << " seconds." << std::endl;
 	
 	std::chrono::duration<double> oldSSSE3 = testOld<SSSE3_8bit>(1, 64, 8);
 	for (int i = 0; i<repeat; i++) {
@@ -116,6 +161,18 @@ int main(int argc, char ** argv) {
 	}
 
 	std::cout << repeat << " iterations of New SSSE3 took: " << newTimeSSSE3.count() << " seconds." << std::endl;
+
+	std::chrono::duration<double> oldAVX2_nobias = testOld_nobias<AVX2_8bit>(1, 64, 8);
+	for (int i = 0; i<repeat; i++) {
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(8, 256, 256);
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(8, 2048, 256);
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(320, 256, 256);
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(472, 256, 256);
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(248, 256, 256);
+		oldAVX2_nobias += testOld_nobias<AVX2_8bit>(200, 256, 256);
+	}
+
+	std::cout << repeat << " iterations of Old AVX2 without bias took: " << oldAVX2_nobias.count() << " seconds." << std::endl;
 
 	std::chrono::duration<double> oldAVX2 = testOld<AVX2_8bit>(1, 64, 8);
 	for (int i = 0; i<repeat; i++) {
