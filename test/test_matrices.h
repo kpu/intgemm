81bfb8b293a3773653ea0f2fdeb372ef583501d7
Mateusz Chudyk <mateuszchudyk@gmail.com>
2020-05-07 19:25:30 +0100

    Make TestMatrices8 more flexible

--- test/test_matrices.h
+++ test/test_matrices.h
@@ -9,34 +9,34 @@
 
 namespace intgemm {
 
-struct TestMatrices8 {
-  typedef Access<RowMajorAccess<int8_t>, ColMajorAccess<int8_t>, RowMajorAccess<int32_t> > AccessT;
+template <typename AccessA, typename AccessB, typename AccessC>
+struct TestMatrices {
+  typedef Access<AccessA, AccessB, AccessC> AccessT;
 
-  explicit TestMatrices8(Tile shape_in) :
+  explicit TestMatrices(Tile shape_in) :
     shape(shape_in),
     A(shape.A_rows * shape.inner),
     B(shape.inner * shape.B_cols),
     C(shape.A_rows * shape.B_cols) {
 
     std::mt19937 gen;
-    std::uniform_int_distribution<int8_t> dist(-127,127);
-    for (int8_t &it : A) it = dist(gen);
-    for (int8_t &it : B) it = dist(gen);
+    std::uniform_int_distribution<typename AccessA::Content> dist(-127,127);
+    for (auto &it : A) it = dist(gen);
+    for (auto &it : B) it = dist(gen);
     // C is uninitialized.
   }
 
   AccessT Accessor() {
-    return AccessT(
-      RowMajorAccess<int8_t>(A.begin(), shape.inner),
-      ColMajorAccess<int8_t>(B.begin(), shape.inner),
-      RowMajorAccess<int32_t>(C.begin(), shape.B_cols));
+    return AccessT({A.begin(), shape.inner}, {B.begin(), shape.inner}, {C.begin(), shape.B_cols});
   }
 
   Tile shape;
-  AlignedVector<int8_t> A;
-  AlignedVector<int8_t> B;
+  AlignedVector<typename AccessA::Content> A;
+  AlignedVector<typename AccessB::Content> B;
   // Uninitialized; for using tests to write to.
-  AlignedVector<int32_t> C;
+  AlignedVector<typename AccessC::Content> C;
 };
+
+using TestMatrices8 = TestMatrices<RowMajorAccess<int8_t>, ColMajorAccess<int8_t>, RowMajorAccess<int32_t>>;
 
 } // namespace intgemm
