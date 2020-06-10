4f46fbd931aa8ee37fa66c154db137e99dbe1c0c
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-06-10 18:35:42 +0200

    Add missing ifdef around AVX512 types

--- vec_traits.h
+++ vec_traits.h
@@ -23,11 +23,13 @@ template <> struct vector_s<CPUType::AVX2, int16_t> { using type = __m256i; };
 template <> struct vector_s<CPUType::AVX2, int> { using type = __m256i; };
 template <> struct vector_s<CPUType::AVX2, float> { using type = __m256; };
 template <> struct vector_s<CPUType::AVX2, double> { using type = __m256d; };
+#ifdef INTGEMM_COMPILER_SUPPORTS_AVX512BW
 template <> struct vector_s<CPUType::AVX512BW, int8_t> { using type = __m512i; };
 template <> struct vector_s<CPUType::AVX512BW, int16_t> { using type = __m512i; };
 template <> struct vector_s<CPUType::AVX512BW, int> { using type = __m512i; };
 template <> struct vector_s<CPUType::AVX512BW, float> { using type = __m512; };
 template <> struct vector_s<CPUType::AVX512BW, double> { using type = __m512d; };
+#endif
 
 template <CPUType CPUType_, typename ElemType_>
 using vector_t = typename vector_s<CPUType_, ElemType_>::type;
